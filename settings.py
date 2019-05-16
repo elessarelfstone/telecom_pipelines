5e73b05ca415011cd7e3d2f14791fa4c91dcc762
dunedain <elessarelfstone@mail.ru>
2019-05-16 21:47:38 +0500

    Fixed ftp loading

--- settings.py
+++ settings.py
@@ -5,18 +5,8 @@ from os.path import expanduser
 from dotenv import load_dotenv
 
 ROOT = path.abspath(path.dirname(__file__))
-ENVS_ROOT = path.join(ROOT, 'env')
 
-DEV_ENV_DIR = path.join(ENVS_ROOT, 'dev', '.env')
-PROD_ENV_DIR = path.join(ENVS_ROOT, 'prod', '.env')
-
-# os.environ["ENV"] = "dev"
-
-if os.environ["ENV"] == "dev":
-    load_dotenv(DEV_ENV_DIR)
-else:
-    load_dotenv(PROD_ENV_DIR)
-
+load_dotenv()
 
 # paths for data
 TEMP_PATH = path.join(expanduser('~'), os.getenv("TEMP_DIR"))
